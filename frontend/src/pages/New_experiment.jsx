abfd1b41951ff099b2ec8e9db295370feb487755
Użytkownik <naciorrozyc@gmail.com>
2026-05-16 05:00:20 +0200

    Modyfikacja formularza dodawania eksperymentu

--- frontend/src/pages/New_experiment.jsx
+++ frontend/src/pages/New_experiment.jsx
@@ -32,7 +32,7 @@ function New_experiment() {
       collaborators: []
     };
 
-    console.log("Wysy³ane dane:", newExperiment);
+    console.log("Wysyï¿½ane dane:", newExperiment);
 
     fetch("http://localhost:8000/api/experiments/", {
       method: "POST",
@@ -51,7 +51,7 @@ function New_experiment() {
       })
       .catch((err) => {
         console.error("Error creating experiment:", err);
-        alert("Coœ posz³o nie tak.");
+        alert("Coï¿½ poszï¿½o nie tak.");
       });
   };
 
@@ -70,89 +70,104 @@ function New_experiment() {
       </header>
 
       <div className="form">
-        <h2>Add a new experiment</h2>
-
-
-        <p>Experiment name:</p>
-        <input 
-            type="text" 
-            placeholder="Name your experiment" 
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-        />
-
-        <p>Plant type:</p>
-        <input 
-            type="text" 
-            placeholder="Type in type of plant" 
-            value={plantName}
-            onChange={(e) => setPlantName(e.target.value)}
-        />
-
-        <p>Experiment description:</p>
-        <input 
-            type="text" 
-            placeholder="Describe your experiment (optional)" 
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-        />
-
-        <p>Tags:</p>
-
-        <p>
-          <label htmlFor="start_date">Start date:</label>
-          <input 
-            type="date" 
-            id="start_date" 
-            name="start_date" 
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
-          />
-        </p>
-
-        <p>
-          <label htmlFor="end_date">Planned end date:</label>
-          <input 
-            type="date" 
-            id="end_date" 
-            name="end_date" 
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
-          />
-        </p>
-
-        <p>Select available setups of sensors you would like to use:</p>
-
-        <div className="setup-container">
-          {sensorSetups.map((setup) => (
-            <div 
-              key={setup.id}
-              className={`setup-card ${selectedSetup === setup.id ? 'selected' : ''}`}
-              onClick={() => setSelectedSetup(setup.id)}
-            >
-              <h3>{setup.title}</h3>
-              <p>{setup.desc}</p>
-            </div>
-          ))}
+        <div className="new-exp-form">
+          <h2>Add a new experiment</h2>          
         </div>
 
-        <p>Collabolators:</p>
 
-        <p>
+        <div className="form-section">
+          <p>Experiment name:</p>
+          <input 
+              type="text" 
+              placeholder="Name your experiment" 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className="form-section">
+          <p>Plant type:</p>
+          <input 
+              type="text" 
+              placeholder="Type in type of plant" 
+              value={plantName}
+              onChange={(e) => setPlantName(e.target.value)}
+          />
+        </div>
+        
+        <div className="form-section">
+          <p>Experiment description:</p>
+          <input 
+              type="text" 
+              placeholder="Describe your experiment (optional)" 
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="form-section">
+          <p>Tags:</p>
+        </div>
+
+
+        <div className="dates-choices">
+          <div className="date-choice">
+            <label htmlFor="start_date">Start date:</label>
+            <input 
+              type="date" 
+              id="start_date" 
+              name="start_date" 
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </div>
+
+          <div className="date-choice">
+            <label htmlFor="end_date">Planned end date:</label>
+            <input 
+              type="date" 
+              id="end_date" 
+              name="end_date" 
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="form-section">
+          <p>Select available setups of sensors you would like to use:</p>
+
+          <div className="setup-container">
+            {sensorSetups.map((setup) => (
+              <div 
+                key={setup.id}
+                className={`setup-card ${selectedSetup === setup.id ? 'selected' : ''}`}
+                onClick={() => setSelectedSetup(setup.id)}
+              >
+                <h3>{setup.title}</h3>
+                <p>{setup.desc}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        
+        <div className="add-collab">
+          <p>Collabolators:</p>
+        </div>
+        <div className="is-public">
             <label htmlFor="experiment_public">
                <input type="checkbox" id="experiment_public" name="experiment_public" value="true" />
                Make my experiment public and let other users see the data.
             </label>
-        </p>
+        </div>
         
 
-          <button 
+          <button className="btn-back"
               onClick={() => navigate('/dashboard')}
           >
               <span>BACK</span>
           </button>
 
-          <button onClick={handleCreate}>
+          <button className="btn-create" onClick={handleCreate}>
               <span>CREATE EXPERIMENT</span>
           </button>
 
