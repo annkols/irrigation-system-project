26c4046b04eaac353fdb76c78acccaa0a7e1190d
Zuzanna Sw <zuzswo@st.amu.edu.pl>
2026-05-29 14:05:30 +0200

    Usunięcie widoku Experiment_details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Start from './pages/Start'; //strona przed logowaniem
 import Dashboard from './pages/Dashboard'; //strona po zalogowaniu (wczesniej App.jsx)
 import New_experiment from './pages/New_experiment'; //dodawanie nowego eksperymentu
-import Experiment_details from './pages/Experiment_details'; //szczegoly eksperymentu
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<Start />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/new-experiment" element={<New_experiment />} />
-        <Route path="/experiment/:id" element={<Experiment_details />} />
       </Routes>
     </Router>
   );
