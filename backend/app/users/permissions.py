41d6c3c32105c35c6740aca7d49b71575deb39e8
naciorrozyc2004 <naciorrozyc@gmail.com>
2026-09-25 20:29:44 +0200

    Update permissions.py (people search)

--- backend/app/users/permissions.py
+++ backend/app/users/permissions.py
@@ -14,12 +14,9 @@ class IsSuperUser(BasePermission):
 
 class CanViewUsers(BasePermission):
     def has_permission(self, request, view):
-        permission = (f"{User._meta.app_label}.view_{User._meta.model_name}")
-
         return (
-            request.user.is_authenticated
-            and request.user.is_staff
-            and request.user.has_perm(permission)
+            request.user 
+            and request.user.is_authenticated
         )
 
 class CanChangeUsers(BasePermission):
